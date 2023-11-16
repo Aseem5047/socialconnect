2a4b6172a468fbdfbf6f1cba7459993f242ea899
Aseem5047 <aseemgupta2002@gmail.com>
2023-11-16 16:10:58 +0530

    final

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -62,7 +62,7 @@ const SignInForm = () => {
 	return (
 		<Form {...form}>
 			<div className="sm:w-420 flex-center flex-col">
-				<img src="/assets/images/logo.svg" alt="logo" />
+				<img src="/assets/images/logo.png" alt="logo" />
 				<h2 className="h3-bold md:h2-bold pt-5 sm:pt-8 ">
 					Authenticate Your Account
 				</h2>
